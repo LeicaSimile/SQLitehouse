4d6b86f2662c2816f643139b0509aeae5516c1ca
Angelica Catalan <tumthe3@gmail.com>
2017-07-09 19:50:35 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name="sqlitereader",
+      version="1.0",
+      description="Simple functions for getting records from a SQLite database.",
+      author="Angelica Catalan",
+      py_modules=["sqlitereader"],
+      license="MIT"
+     )
