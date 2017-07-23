f79269035bf7ce8148440240eb7b4b400dfd934d
Angelica Catalan <tumthe3@gmail.com>
2017-07-23 14:27:13 -0400

    Add email, url

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(name="sqlitereader",
       version="1.0",
       description="Simple functions for getting records from a SQLite database.",
       author="Tumthe3",
+      author_email="tumthe3@gmail.com",
+      url="https://github.com/Tumthe3/SQLite-Reader",
       py_modules=["sqlitereader"],
       license="MIT"
      )
