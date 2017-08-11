7ded9f8b98a28bb289c959e496958e1a71c24a67
Angelica Catalan <tumthe3@gmail.com>
2017-08-11 12:36:51 -0400

    Change to version 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="sqlitehouse",
-      version="1.0",
+      version="1.1",
       description="Simple library for reading/writing records in a SQLite database.",
       author="Tumthe3",
       author_email="tumthe3@gmail.com",
