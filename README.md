e25551674cb6d415ec09b0e4d74ef89ddaeb41bb
Tumthe3 <tumthe3@gmail.com>
2017-10-07 15:55:39 -0400

    Update username/email

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple library for reading/writing records in a SQLite database.
 
 ## Authors
-* Angelica Catalan ([Tumthe3](https://github.com/Tumthe3))
+* Angelica Catalan ([LeicaSimile](https://github.com/LeicaSimile))
 
 ## License
 Hooray, it's the [MIT License](LICENSE.md). Have fun.
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(name="sqlitehouse",
       version="1.2",
       description="Simple library for reading/writing records in a SQLite database.",
-      author="Tumthe3",
-      author_email="tumthe3@gmail.com",
-      url="https://github.com/Tumthe3/SQLitehouse",
+      author="LeicaSimile",
+      author_email="leicasimile@gmail.com",
+      url="https://github.com/LeicaSimile/SQLitehouse",
       py_modules=["sqlitehouse"],
       license="MIT"
      )
